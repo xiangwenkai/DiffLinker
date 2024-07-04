ecdb53366cfcb1b4a8563d348e2ff46cfd287d5b
xiangwenkai <icoxia@gmail.com>
2024-07-04 15:29:23 +0800

    update

--- src/visualizer.py
+++ src/visualizer.py
@@ -223,6 +223,5 @@ def visualize_chain(
     gif_path = dirname + '/output.gif'
     imageio.mimsave(gif_path, imgs, subrectangles=True)
 
-    wandb = None
     if wandb is not None:
         wandb.log({mode: [wandb.Video(gif_path, caption=gif_path)]})
